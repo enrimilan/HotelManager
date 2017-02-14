bd65dc4eb78f50aa998925355db677903bd1ab84
Enri Miho <enrimiho@gmail.com>
2017-02-14 19:25:41 +0100

    Add RoomService and implementation.

---
+++ HotelManager/Service/Impl/RoomServiceImpl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using HotelManager.Entity;
+
+namespace HotelManager.Service.Impl
+{
+    class RoomServiceImpl : RoomService
+    {
+        public Room create(Room room)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Room delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void edit(Room room)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Room getRoom(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HotelManager/Service/RoomService.cs
@@ -0,0 +1,14 @@
+﻿using HotelManager.Entity;
+
+namespace HotelManager.Service
+{
+    interface RoomService
+    {
+
+        Room create(Room room);
+        Room getRoom(int id);
+        void edit(Room room);
+        Room delete(int id);
+
+    }
+}
