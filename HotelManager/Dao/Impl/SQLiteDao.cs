1514da9283b4bd176cc8e54b94375150b3a90c40
Enri Miho <enrimiho@gmail.com>
2017-02-15 02:04:30 +0100

    SQLiteDao base class for all daos (includes method to execute a sql query).

---
+++ HotelManager/Dao/Impl/SQLiteDao.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+using System.Data.SQLite;
+
+namespace HotelManager.Dao.Impl
+{
+    public class SQLiteDao
+    {
+        protected SQLiteConnection connection;
+
+        public SQLiteDao()
+        {
+            connection = DbConnection.GetConnection();
+        }
+
+        public DataTable ExecuteSql(string query)
+        {
+            SQLiteCommand cmd = new SQLiteCommand(connection);
+            cmd.CommandText = query + ";";
+            DataTable dt = new DataTable();
+            SQLiteDataReader reader = cmd.ExecuteReader();
+            dt.Load(reader);
+            reader.Close();
+            return dt;
+        }
+
+    }
+}
