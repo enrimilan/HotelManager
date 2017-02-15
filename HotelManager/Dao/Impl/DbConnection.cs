7cbd5532f75f9eab4aec83af0c2145727709dfb8
Enri Miho <enrimiho@gmail.com>
2017-02-15 02:01:50 +0100

    Add a singleton class for getting the database connection.

---
+++ HotelManager/Dao/Impl/DbConnection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace HotelManager.Dao
+{
+    public class DbConnection
+    {
+        private static SQLiteConnection connection = null;
+
+        private DbConnection()
+        {
+            string relativePath = @"Database\hotel.s3db";
+            string currentPath;
+            currentPath = AppDomain.CurrentDomain.BaseDirectory;
+            currentPath = currentPath.Substring(0, currentPath.Length - 11);
+            string absolutePath = System.IO.Path.Combine(currentPath, relativePath);
+            string connectionString = string.Format("Data Source={0}", absolutePath);
+            connection = new SQLiteConnection(connectionString);
+            connection.Open();
+        }
+
+        public static SQLiteConnection GetConnection()
+        {
+            if (connection == null)
+            {
+                new DbConnection();
+            }
+            return connection;
+        }
+    }
+}
