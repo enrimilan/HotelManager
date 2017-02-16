3bbc832ca7e6cfbf62b1c2aeb9e680cb377de2d2
Enri Miho <enrimiho@gmail.com>
2017-02-16 16:31:46 +0100

    Add a xaml for App.cs

---
+++ HotelManager/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using HotelManager.Entity;
+using HotelManager.Service;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace HotelManager
+{
+    public partial class App : Application
+    {
+
+        [System.STAThreadAttribute()]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.0.0.0")]
+        public static void Main()
+        {
+            RoomService roomService = ServiceFactory.GetRoomService();
+            List<Room> rooms = roomService.FindAllRooms();
+            foreach (Room room in rooms)
+            {
+                System.Diagnostics.Debug.WriteLine("Record: id=" + room.Id + " name=" + room.Number);
+            }
+
+            App app = new App();
+            app.InitializeComponentCustom();
+            app.Run();
+        }
+
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.0.0.0")]
+        public void InitializeComponentCustom()
+        {
+            this.StartupUri = new System.Uri("Gui/Main.xaml", System.UriKind.Relative);
+        }
+
+    }
+}
