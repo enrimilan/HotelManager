10dc64fb6ac26bdb8c72c0e8b93cc2d9abe84a43
Enri Miho <enrimiho@gmail.com>
2017-02-18 01:06:57 +0100

    Creation dialogs for rooms

---
+++ HotelManager/Gui/Dialog/CreateRoomDialog.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HotelManager.Gui.Dialog
+{
+    /// <summary>
+    /// Interaction logic for CreateRoomDialog.xaml
+    /// </summary>
+    public partial class CreateRoomDialog : Window
+    {
+        public CreateRoomDialog()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// CloseButton_Clicked
+        /// </summary>
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+    }
+}
