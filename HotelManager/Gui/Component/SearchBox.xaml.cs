610f2cfb37b966a3d5b93d7948f27aec512f6558
Enri Miho <enrimiho@gmail.com>
2017-02-18 12:52:06 +0100

    Add a search control.

---
+++ HotelManager/Gui/Component/SearchBox.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace HotelManager.Gui.Component
+{
+    /// <summary>
+    /// Interaction logic for SearchBox.xaml
+    /// </summary>
+    public partial class SearchBox : UserControl
+    {
+
+        public SearchBox()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var t = (TextBox)sender;
+            t.SelectAll();
+        }
+
+        private void SearchTextBox_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            var t = (TextBox)sender;
+            t.SelectAll();
+        }
+    
+    }
+}
