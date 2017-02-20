36a69c051289508cdf19467ecf4ee096c5ebd4ba
Enri Miho <enrimiho@gmail.com>
2017-02-21 00:38:11 +0100

    Entity for reservations.

---
+++ HotelManager/Entity/Reservation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HotelManager.Entity
+{
+    public class Reservation
+    {
+
+        public int Id { get; set; }
+        public DateTime From { get; set; }
+        public string FromDateString { get; set; }
+        public DateTime To { get; set; }
+        public string ToDateString { get; set; }
+        public string Person { get; set; }
+        public string Contact { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string CreationDateString { get; set; }
+        public DateTime EndDate { get; set; }
+        public string EndDateString { get; set; }
+        public bool Canceled { get; set; }
+        public bool Past { get; set; }
+        public bool CheckedIn { get; set; }
+        public string Status { get; set; }
+        public Room Room { get; set; }
+        public string RoomString { get; set; }
+        
+    }
+}
