8f94793017141b61c32614dac7698f5ab47d6705
Enri Miho <enrimiho@gmail.com>
2017-02-21 23:29:48 +0100

    add new constant

--- HotelManager/Util/Constants.cs
+++ HotelManager/Util/Constants.cs
@@ -4,5 +4,6 @@ namespace HotelManager.Util
     public static class Constants
     {
         public const string DateFormat = "yyyy.MM.dd HH:mm:ss";
+        public const string DateFormatWithoutTime = "yyyy.MM.dd";
     }
 }
